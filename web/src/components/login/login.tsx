68659c0f29089c12d56a6c8c5eba7d8e6adc4437
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2021-01-27 00:17:17 +0900

    fixed login system
    session, token

--- web/src/components/login/login.tsx
+++ web/src/components/login/login.tsx
@@ -1,26 +1,8 @@
 import * as React from 'react'
 import { auth, provider, session } from './firebaseConfig'
 
-var user;
 
-const SignInWithGoogle = () => {
-    auth.setPersistence(session).then(()=>  {
-        auth.signInWithPopup(provider).then(() => {
-            user = auth.currentUser;
-            console.log("login: ", user);
-        })
-        console.log("auth: ", auth.currentUser);
-        console.log(user);
-    })
-}
-const SignOut = () => {
-    auth.signOut().then(() => {
-        user = auth.currentUser;
-    })
-    console.log(user);
-}
-
-export class LogIn extends React.Component {
+export class LogIn extends React.Component<any, any> {
 
     constructor(props: any) {
         super(props);
@@ -28,26 +10,51 @@ export class LogIn extends React.Component {
         this.state = {
             user: null
         }
+
+        this.SignInWithGoogle = this.SignInWithGoogle.bind(this);
+        this.SignOut = this.SignOut.bind(this);
+    }
+
+
+    SignInWithGoogle = () => {
+        auth.setPersistence(session).then(()=>  {
+            auth.signInWithPopup(provider).then(() => {
+                this.setState({user: auth.currentUser});
+                console.log("login: ", this.state.user);
+            })
+            console.log("auth: ", auth.currentUser);
+            console.log(this.state.user);
+        })
+    }
+    SignOut = () => {
+        auth.signOut().then(() => {
+            this.setState({user: auth.currentUser});
+        })
+        console.log(this.state.user);
     }
 
     render() {
-        console.log("current: ", user);
+        console.log("current: ", this.state.user);
         return (
             <div className="LogIn">
                 <header>
                     {
-                        user
-                        ? <p>Hello, {user.displayName}</p>
+                        this.state.user
+                        ? <p>Hello, {this.state.user.displayName}</p>
                         : <p>Please sign in.</p>
                     }
 
                     {
-                        user
-                        ? <button onClick={SignOut}>Sign out</button>
-                        : <button onClick={SignInWithGoogle}>Sign in with Google</button>
+                        this.state.user
+                        ? <button onClick={this.SignOut}>Sign out</button>
+                        : <button onClick={this.SignInWithGoogle}>Sign in with Google</button>
                     }
                 </header>
             </div>
         );
     }
+
+    /*private componentDidUpdate() {
+        this.render();
+    }*/
 }
